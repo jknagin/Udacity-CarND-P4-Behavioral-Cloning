c95ad88e4d7014c5d648322de060b9be4d5d0751
Jay Nagin <jknagin@gmail.com>
2020-12-16 19:55:56 -0800

    Added to model.py

--- model.py
+++ model.py
@@ -1,47 +1,112 @@
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten, Dropout
-from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPooling2D
-import pickle
+from keras.layers.core import Dense, Activation, Flatten
+from keras.layers import BatchNormalization
+from keras.layers.convolutional import Convolution2D
+from keras.layers import Lambda, Cropping2D
 import numpy as np
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
+import cv2
+import time
+from PIL import Image
 
-# Build the Final Test Neural Network in Keras Here
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dense(5))
-model.add(Activation('softmax'))
 
-# preprocess training data
-X_normalized = np.array(X_train / 255.0 - 0.5)
-label_binarizer = LabelBinarizer()
-y_one_hot = label_binarizer.fit_transform(y_train)
+def read_image(fname: str) -> np.ndarray:
+    return np.array(Image.open(fname))
 
-# compile and fit the model
-model.compile('adam', 'categorical_crossentropy', ['accuracy'])
-history = model.fit(X_normalized, y_one_hot, epochs=10, validation_split=0.2)
 
-# evaluate model against the test data
-with open('small_test_traffic.p', 'rb') as f:
-    data_test = pickle.load(f)
+def main():
+    # Seed RNG
+    np.random.seed(1)
 
-X_test = data_test['features']
-y_test = data_test['labels']
+    # Load data
+    print("Loading data...")
 
-# preprocess test data
-X_normalized_test = np.array(X_test / 255.0 - 0.5 )
-y_one_hot_test = label_binarizer.fit_transform(y_test)
+    data_directory = os.path.join("..", "data")
+    driving_log = pd.read_csv(os.path.join(data_directory, "driving_log.csv")).values
+    X = []
+    y = []
+    steering_correction = 0.2
+    for idx in range(driving_log.shape[0]):
+        try:
+            steering = driving_log[idx, 3]
+            # Center
+            X.append(read_image(os.path.join(data_directory, driving_log[idx, 0].strip())))
+            y.append(steering)
+            # Left
+            X.append(read_image(os.path.join(data_directory, driving_log[idx, 1].strip())))
+            y.append(steering + steering_correction)
+            # Right
+            X.append(read_image(os.path.join(data_directory, driving_log[idx, 2].strip())))
+            y.append(steering - steering_correction)
+        except:
+            print("Failed to load row {}".format(idx))
 
-print("Testing")
+    # print("Augmenting data...")
+    # # Augment via horizontal flip, negate steering
+    # original_data_length = len(X)
+    # for i in range(original_data_length):
+    #     X.append(cv2.flip(X[i], 1))  # flip horizontally
+    #     y.append(-1.0 * y[i])
 
-metrics = model.evaluate(X_normalized_test, y_one_hot_test)
-for metric_i in range(len(model.metrics_names)):
-    metric_name = model.metrics_names[metric_i]
-    metric_value = metrics[metric_i]
-    print('{}: {}'.format(metric_name, metric_value))
+    # Convert images and steering data to numpy arrays
+    X_train = np.array(X)
+    y_train = np.array(y)
+
+    # Train/validation/test split
+    # train_split = 0.99
+    valid_split = 0.2
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 - train_split, random_state=0)
+
+    # Preprocessing function
+    def preprocessing(x):
+        return x / 255.0 - 0.5
+
+    # Build the Final Test Neural Network in Keras Here
+    model = Sequential()
+    model.add(Lambda(preprocessing, input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(BatchNormalization())
+    model.add(Dense(50))
+    model.add(BatchNormalization())
+    model.add(Dense(10))
+    model.add(BatchNormalization())
+    model.add(Dense(1))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Dense(1))
+
+    # compile and fit the model
+    model.compile(loss="mse", optimizer="adam")
+
+    history = model.fit(X_train, y_train, epochs=5, validation_split=valid_split, shuffle=True, verbose=1)
+
+    # Save model
+    model_filename = "model.h5"
+    model.save(model_filename)
+    print("{} saved".format(model_filename))
+
+    # Test model
+    # metrics = model.evaluate(X_test, y_test)
+    # for metric_i in range(len(model.metrics_names)):
+    #     metric_name = model.metrics_names[metric_i]
+    #     metric_value = metrics[metric_i]
+    #     print('{}: {}'.format(metric_name, metric_value))
+
+
+if __name__ == "__main__":
+    main()
